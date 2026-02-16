29c6f0bb3db2285bf73b6f76681a2ab26be435d1
Kristannnnn <aragonjames111@gmail.com>
2026-02-16 17:17:42 +0800

    chore: fix auth problems and added token

--- src/controllers/logInController.ts
+++ src/controllers/logInController.ts
@@ -1,4 +1,5 @@
 import User from "@/models/Users";
+import { generateToken } from "@/utils/error/generateToken";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 
@@ -11,22 +12,22 @@ export const logInUser = async (req: Request, res: Response) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    const passwordMatch = await bcrypt.compare(
-      password,
-      user.passwordHash,
-    );
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Invalid password" });
     }
-    res.json({ 
-      message: "Login successful", 
+    const token = generateToken(user._id.toString());
+    res.json({
+      message: "Login successful",
+      token,
       user: {
         id: user._id,
         username: user.username,
-        email: user.email
-      }
+        email: user.email,
+      },
     });
   } catch (err) {
-    res.status(400).json({ message: "Error logging in user", error: err });
+    console.error("LOGIN ERROR:", err);
+    res.status(400).json({ message: "Error logging in user" });
   }
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,6 +5,7 @@ import {
   getUser,
   updateUser,
 } from "@/controllers/userController";
+import { authMiddleware } from "@/middlewares/auth";
 import express from "express";
 
 const router = express.Router();
@@ -12,7 +13,7 @@ const router = express.Router();
 router.post("/", createUser);
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.put("/:id", authMiddleware, updateUser);
+router.delete("/:id", authMiddleware, deleteUser);
 
 export default router;
